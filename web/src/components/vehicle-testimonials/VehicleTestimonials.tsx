c84d964be85d2fbc5c2aa70276a590602dd7bd97
chrismpettyjohn <chrismpettyjohn@gmail.com>
2024-12-23 14:43:47 -0500

    fix: extra testimonials static deleted

--- web/src/components/vehicle-testimonials/VehicleTestimonials.tsx
+++ web/src/components/vehicle-testimonials/VehicleTestimonials.tsx
@@ -1,5 +1,4 @@
 import { useVehicle } from "../../contexts/VehicleContext";
-import { formatNumber } from "../../hooks/formatNumber";
 import { useVehicleTestimonies } from "../../hooks/useVehicle";
 
 export function VehicleTestimonials() {
@@ -16,12 +15,6 @@ export function VehicleTestimonials() {
                         </div>
                     ))
                 }
-                <div className="testimonial">
-                    "Great reliable car, had it for 5 years with minimal issues. Regular maintenance is key."
-                </div>
-                <div className="testimonial">
-                    "Decent fuel economy and comfortable for long trips. Watch out for the timing belt though."
-                </div>
             </div>
         </div>
     )
